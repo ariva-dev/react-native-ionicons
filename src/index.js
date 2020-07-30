fee16abd7276b24753c6712ca5f2cae7a807d1bc
ariva-dev <ariva.dev@gmail.com>
2020-07-30 07:13:52 +0700

    Add default value for strokeWidth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import {Icons} from "./map"
 
 export function Icon({name, size, color, fill, strokeWidth, style, borderWidth}) {
+  const strokeWidthValue = strokeWidth ? strokeWidth : 32
   return Icons[name]({
     height: size,
     width: size,
     fill,
     color,
     style,
-    strokeWidth
+    strokeWidth: strokeWidthValue
    // borderWidth: borderWidth
   });
 }
